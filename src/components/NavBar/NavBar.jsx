589610320029208be3a921594bf38a38fabb7cc1
vanessaj <101592196+vns-shanshan@users.noreply.github.com>
2024-10-15 07:49:59 -0700

    Sidebar (#1)
    * Added css for navbar
    
    * Clean up

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,33 +1,90 @@
-import { Link } from 'react-router-dom';
-import { AuthedUserContext } from '../../App';
-import { useContext } from 'react';
+import { Link, useLocation } from "react-router-dom";
+import { AuthedUserContext } from "../../App";
+import { useContext } from "react";
+
+import styles from "./NavBar.module.css";
 
 const NavBar = ({ handleSignout }) => {
   const user = useContext(AuthedUserContext);
+  const location = useLocation();
+  const selectedTab = location.pathname;
+
   return (
     <>
       {user ? (
-        <nav>
+        <nav className={styles.container}>
           <ul>
-            <li>Welcome, {user.username}</li>
             <li>
-              <Link to="/">Dashboard</Link>
+              <div
+                className={
+                  selectedTab === "/" ? styles.linkBgSelected : styles.linkBg
+                }
+              >
+                <Link to="/">Home</Link>
+              </div>
             </li>
             <li>
-              <Link to="" onClick={handleSignout}>
-                Sign Out
-              </Link>
+              <div
+                className={
+                  selectedTab === "/pinstas/new"
+                    ? styles.linkBgSelected
+                    : styles.linkBg
+                }
+              >
+                <Link to="/pinstas/new">Create</Link>
+              </div>
+            </li>
+            <li>
+              <div
+                className={
+                  selectedTab === `/profiles/${user._id}`
+                    ? styles.linkBgSelected
+                    : styles.linkBg
+                }
+              >
+                <Link to={`/profiles/${user._id}`}>Profile</Link>
+              </div>
+            </li>
+            <li>
+              <div>
+                <Link onClick={handleSignout}>Sign Out</Link>
+              </div>
             </li>
           </ul>
         </nav>
       ) : (
-        <nav>
+        <nav className={styles.container}>
           <ul>
             <li>
-              <Link to="/signin">Sign In</Link>
+              <div
+                className={
+                  selectedTab === "/" ? styles.linkBgSelected : styles.linkBg
+                }
+              >
+                <Link to="/">Home</Link>
+              </div>
             </li>
             <li>
-              <Link to="/signup">Sign Up</Link>
+              <div
+                className={
+                  selectedTab === "/signin"
+                    ? styles.linkBgSelected
+                    : styles.linkBg
+                }
+              >
+                <Link to="/signin">Log In</Link>
+              </div>
+            </li>
+            <li>
+              <div
+                className={
+                  selectedTab === "/signup"
+                    ? styles.linkBgSelected
+                    : styles.linkBg
+                }
+              >
+                <Link to="/signup">Sign Up</Link>
+              </div>
             </li>
           </ul>
         </nav>
