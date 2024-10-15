c5e1c1bfaa8fa906a0ac4a1460400c941a2b98bc
huijingj <vanessajin0426@gmail.com>
2024-10-14 23:11:40 -0700

    Added css for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState, createContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
-import Landing from './components/Landing/Landing';
-import Dashboard from './components/Dashboard/Dashboard';
-import SignupForm from './components/SignupForm/SignupForm';
-import SigninForm from './components/SigninForm/SigninForm';
-import * as authService from '../src/services/authService'; // import the authservice
+import { useState, createContext } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
+
+import NavBar from "./components/NavBar/NavBar";
+import Landing from "./components/Landing/Landing";
+import Dashboard from "./components/Dashboard/Dashboard";
+import SignupForm from "./components/SignupForm/SignupForm";
+import SigninForm from "./components/SigninForm/SigninForm";
+import * as authService from "../src/services/authService"; // import the authservice
 
 export const AuthedUserContext = createContext(null);
 
@@ -20,16 +23,18 @@ const App = () => {
   return (
     <>
       <AuthedUserContext.Provider value={user}>
-        <NavBar user={user} handleSignout={handleSignout} />
-        <Routes>
-          {user ? (
-            <Route path="/" element={<Dashboard user={user} />} />
-          ) : (
-            <Route path="/" element={<Landing />} />
-          )}
-          <Route path="/signup" element={<SignupForm setUser={setUser} />} />
-          <Route path="/signin" element={<SigninForm setUser={setUser} />} />
-        </Routes>
+        <div id="app-container">
+          <NavBar user={user} handleSignout={handleSignout} />
+          <Routes>
+            {user ? (
+              <Route path="/" element={<Dashboard user={user} />} />
+            ) : (
+              <Route path="/" element={<Landing />} />
+            )}
+            <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+            <Route path="/signin" element={<SigninForm setUser={setUser} />} />
+          </Routes>
+        </div>
       </AuthedUserContext.Provider>
     </>
   );
