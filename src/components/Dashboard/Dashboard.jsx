5917521f93d9f26c960b4f9a6a13ebd907a10465
DannyR716 <01dromero10@gmail.com>
2024-10-17 11:06:35 -0400

    removed unnecessary imports

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,14 +1,11 @@
 import { AuthedUserContext } from '../../App';
 import { useContext } from 'react';
-import PostSearch from './PostSearch';
-
 
 const Dashboard = () => {
   const user = useContext(AuthedUserContext);
   return (
     <main>
       <h1>Welcome, {user.username}</h1>
-      <PostSearch />
 
       <p>
         This is the dashboard page where you, and only you, can see a dashboard
